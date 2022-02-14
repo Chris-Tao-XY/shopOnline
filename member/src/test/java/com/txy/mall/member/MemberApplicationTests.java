4ffde9ee8932ba7cf702d009d5b274aceeb49434
Chris <txy_0620@163.com>
2022-02-14 22:58:20 +0800

    first init

---
+++ member/src/test/java/com/txy/mall/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.txy.mall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
